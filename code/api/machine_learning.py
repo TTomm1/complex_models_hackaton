5515753f934854cb8e94ff32b450fe22d8fe6022
Tudor Toma <ttoma@Tudors-MacBook-Pro.local>
2017-01-25 13:51:57 +0100

    update

--- code/api/machine_learning.py
+++ code/api/machine_learning.py
@@ -16,6 +16,7 @@ from sklearn.model_selection import GridSearchCV
 
 
 def train_random_forest(X_train, y_train, type="classifier", param_grid=None):
+    grid = None
     if type == "classifier":
         rf = RandomForestClassifier(random_state=7)
     else:
@@ -27,12 +28,12 @@ def train_random_forest(X_train, y_train, type="classifier", param_grid=None):
     return grid
 
 
-def get_random_forest_model(data,labels,type="classifier"):
-    param_grid={
+def get_random_forest_model(data, labels, type = "classifier", test_size=0.25, random_state=42):
+    param_grid = {
         'max_features': np.unique(np.round(np.linspace(0.3, 0.99, 9), 1)),
         'n_estimators': np.arange(100, 1100, 100)
     }
-    X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.25, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=test_size, random_state=random_state)
     model = train_random_forest(X_train, y_train, type=type, param_grid=param_grid)
     return model
 
