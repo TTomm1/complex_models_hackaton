92eabb8557316cd0658c57c56ea18dc44dca61f1
Tudor Toma <ttoma@Tudors-MacBook-Pro.local>
2017-01-25 15:16:22 +0100

    updates

--- code/api/machine_learning.py
+++ code/api/machine_learning.py
@@ -31,7 +31,7 @@ def train_random_forest(X_train, y_train, type="classifier", param_grid=None):
 def get_random_forest_model(data, labels, type = "classifier", test_size=0.25, random_state=42):
     param_grid = {
         'max_features': np.unique(np.round(np.linspace(0.3, 0.99, 9), 1)),
-        'n_estimators': np.arange(100, 1100, 100)
+        'n_estimators': np.arange(100, 1100, 10)
     }
     X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=test_size, random_state=random_state)
     model = train_random_forest(X_train, y_train, type=type, param_grid=param_grid)
